abc38db92ceb7021cb8368913c0df77a882f0870
wendyhamel <whamel@it4web.nl>
2022-05-13 18:03:27 +0200

    Setup and structure, setup for use of tailwind and alpine

---
+++ src/tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        fontFamily : {
+            'sans': ['Barlow', 'sans-serif'],
+            'heading': ['Bellefair', 'sans-serif'],
+            'sub': ['Barlow Condensed', 'sans-serif'],
+        },
+        extend: {
+            colors: {
+                white: '#ffffff',
+                blue: '#d0d6f9',
+                dark: '#0b0d17',
+            },
+
+        }
+    }
+}
