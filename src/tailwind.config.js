61f7ae224fad1485427995f025b1b6006fdf3e46
wendyhamel <whamel@it4web.nl>
2022-05-17 16:28:17 +0200

    Working on replacing arbitrary values

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -6,6 +6,37 @@ tailwind.config = {
             'sub': ['Barlow Condensed', 'sans-serif'],
         },
         extend: {
+            borderWidth: {
+              '3': '3px',
+            },
+            lineHeight: {
+                'xxs': '19px',
+                'xs': '23px',
+                'sm': '25px',
+                'md': '34px',
+                'lg': '100px',
+                'xl': '150px',
+                'xxl': '172px',
+            },
+            letterSpacing: {
+                'xs': '1.25px',
+                'sm': '2px',
+                'md': '2.7px',
+                'lg': '3.38px',
+                'xl': '4.73px',
+            },
+            backdropSaturate: {
+                '125': '1.25',
+            },
+            backdropBlur: {
+                '30': '30px',
+            },
+            opacity: {
+              '4': '.04',
+            },
+            width: {
+                menu: '254px'
+            },
             colors: {
                 white: '#ffffff',
                 blue: '#d0d6f9',
